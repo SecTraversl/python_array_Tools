9b1f69d2f5e0b564db3382b76ada1ed6f9478754
Travis Logue <SecTraversl@ProtonMail.com>
2021-10-22 10:14:59 -0700

    Last write time 2021-10-22

---
+++ clear_array.py
@@ -0,0 +1,14 @@
+# %%
+#######################################
+def clear_array(lst: list):
+    """Takes a given list and clears out all of the elements within the list.
+
+    Examples:
+        >>> mylst\n
+        [('a', 1, 'John'), ('b', 2, 'Alice'), ('c', 3, 'Bob')]
+        >>> clear_array(mylst)\n
+        >>> mylst\n
+        []
+    """
+    return lst.clear()
+
