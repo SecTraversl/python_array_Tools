f27a816afa10e5d2a2c3b5ae93969eee2299f378
Travis Logue <SecTraversl@ProtonMail.com>
2021-10-22 10:15:35 -0700

    Last write time 2021-10-22

---
+++ sum_array.py
@@ -0,0 +1,16 @@
+# %%
+#######################################
+def sum_array(lst: list):
+    """Returns the sum of the numbers in the list.
+
+    Example:
+        >>> list(range(0,10))\n
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        >>> myrange = list(range(0,10))\n
+        >>> sum_array(myrange)\n
+        45
+        >>> sum(list(range(0,10)))\n
+        45
+    """
+    return sum(lst)
+
